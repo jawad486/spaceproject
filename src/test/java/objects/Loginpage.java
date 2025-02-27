8042979a6290c3ca9cf7693aa706c836f0437c53
jawad486 <jawadqureshi486@gmail.com>
2025-02-27 10:11:37 -0500

    2nd commit

--- src/test/java/objects/Loginpage.java
+++ src/test/java/objects/Loginpage.java
@@ -1,5 +1,15 @@
 package objects;
 
-public class Loginpage {
+import org.openqa.selenium.By;
 
+public class Loginpage {
+	
+	By name=By.xpath("//3456");
+	By pass=By.name("qureshi");
+	
+	
+	
+	public void display() {
+		System.out.println("this is my jawad");
+	}
 }
